1edbb736c132d0047d2858b7b25a95051623e9eb
Jasper Hugunin <jasper@hugunin.net>
2021-07-22 20:37:37 -0700

    Show imports

--- src/main.js
+++ src/main.js
@@ -5,3 +5,4 @@ term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ \r\nnew line\r\nthird line'
 
 const justMathModule = await WebAssembly.compileStreaming(fetch('tmp/just_math.wasm'));
 const justMathInstance = await instantiateWithMemory(justMathModule, {env: {}}, {namespace: 'env', name: 'memory'});
+console.log(WebAssembly.Module.imports(justMathModule));
