c7812c7c1af740ceff17e1872180133d01f2478a
Jasper Hugunin <jasper@hugunin.net>
2021-09-18 21:20:40 -0700

    Add function for logging ints

--- src/worker/diagnostic.js
+++ src/worker/diagnostic.js
@@ -2,6 +2,9 @@ const diagnostic = {
   log_syscall: (syscall_num) => {
     console.log(`Syscall ${syscall_num}`);
   },
+  log_i32: (num) => {
+    console.log(`WASM: ${num}/0x${num.toString(16)}`);
+  },
   debugger: () => { debugger; },
 };
 
