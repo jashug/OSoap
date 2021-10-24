d82e6c044da8f58c8e5f7026e54142313fd2fd85
Jasper Hugunin <jasper@hugunin.net>
2021-10-24 08:53:23 -0700

    Better error handling of misformed executables

--- src/util/executableFromBlob.js
+++ src/util/executableFromBlob.js
@@ -21,8 +21,17 @@ const parseShebang = (bytes) => {
 const wasmHeader = new Uint8Array([0x00, 0x61, 0x73, 0x6D]); // \0asm
 const shebangHeader = new Uint8Array([0x23, 0x21]); // #!
 
+const compileWasm = async (backingArray) => {
+  try {
+    return await WebAssembly.compile(backingArray);
+  } catch (e) {
+    if (e instanceof WebAssembly.CompileError) throw new SyscallError(E.NOEXEC);
+    else throw e;
+  }
+};
+
 const executableFromUint8Array = async (array) => {
-  if (compareMagic(array, wasmHeader)) return {done: true, module: await WebAssembly.compile(array)};
+  if (compareMagic(array, wasmHeader)) return {done: true, module: await compileWasm(array)};
   if (compareMagic(array, shebangHeader)) return parseShebang(array);
   throw new SyscallError(E.NOEXEC);
 };
@@ -30,7 +39,7 @@ const executableFromUint8Array = async (array) => {
 // TODO: since compile needs an arraybuffer not a blob, maybe simplest to just go from ArrayBuffer directly.
 const executableFromBlob = async (blob) => {
   const headerBytes = new Uint8Array(await blob.slice(0, Math.min(blob.size, HEADER_SIZE)).arrayBuffer());
-  if (compareMagic(headerBytes, wasmHeader)) return {done: true, module: await WebAssembly.compile(await blob.arrayBuffer())};
+  if (compareMagic(headerBytes, wasmHeader)) return {done: true, module: await compileWasm(await blob.arrayBuffer())};
   if (compareMagic(headerBytes, shebangHeader)) return parseShebang(headerBytes);
   throw new SyscallError(E.NOEXEC);
 };
